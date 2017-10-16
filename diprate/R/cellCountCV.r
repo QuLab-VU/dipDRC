f67260770831b3751bac3e1e3460b87d189ccd99
Darren Tyson - MBP <darren.tyson@vanderbilt.edu>
2017-10-16 00:00:55 -0500

    Starting to document all the functions in the diprate package.

--- diprate/R/cellCountCV.r
+++ diprate/R/cellCountCV.r
@@ -1,65 +1,55 @@
-cellCountCV	<-	function(rawCVdata, normPos=1)	{
-	
-	log2norm <- function(count, ids, norm_idx=1, zero=log2(0.999))
-	{
-		# l2
-		l2 <- log2(count)
-			# finds time points with no cells (0), and replace it
-			# with zero (log2(0.999)), so that the data can be displayed 
-			# in log scale, yet easily found.
-		l2[is.infinite(l2)] <- zero
-	
-		norm <- numeric()
-		group <- as.character(unique(ids))
-		for(i in group)
-		{
-			d <- l2[ids == i]
-			norm <- append(norm, d - d[norm_idx])
-		}
-	
-		norm
-	}
+cellCountCV <- function(rawCVdata, normPos=1) {
+    #' Function to process cell count data in Cellavista multi-measurement format
+    #'
+    #' @param rawCVdata \emph{data.frame}
+    #' @param normPos \emph{integer}
+    #'
+    #' Function takes \code{data.frame} of raw cell count data exported from Synentec
+    #' Cellavista and extracts time of image acquisition, identifies wells from which images
+    #' have been obtained, outputs a \code{data.frame} with \code{colnames} of
+    #' \emph{Row, Column, Well, Cell.count, nl2}. \emph{nl2} is log2 values of \emph{Cell.count}
+    #' normalized to \emph{normPos} argument (default = 1).
 
-	d <- rawCVdata
-	d <- d[,c('Column','Row','Cell.Nucleus')]
-	# rename Cell.Nucleus to Cell.count
-	colnames(d)[3]	<-	'Cell.count'
+    d <- rawCVdata
+    d <- d[,c('Column','Row','Cell.Nucleus')]
+    # rename Cell.Nucleus to Cell.count
+    colnames(d)[3] <- 'Cell.count'
 
-	# ensure that Row and Column are char vectors
-	d$Row		<-	as.character(d$Row)
-	d$Column	<-	as.character(d$Column)
+    # ensure that Row and Column are char vectors
+    d$Row <- as.character(d$Row)
+    d$Column <- as.character(d$Column)
 
-	# make row index of data positions for each time point
-	idx			<-	grep('Timespan',d[,1])
+    # make row index of data positions for each time point
+    idx <- grep('Timespan',d[,1])
 
-	# extract time of acquisition for each time point
-	times		<-	as.numeric(d[,2][idx])
-	time.idx	<-	seq(length(times))
+    # extract time of acquisition for each time point
+    times <- as.numeric(d[,2][idx])
+    time.idx <- seq(length(times))
 
-	# extract the number of wells we have data for
-	numWells	<-	idx[2]-idx[1]-2
+    # extract the number of wells we have data for
+    numWells <- idx[2]-idx[1]-2
 
-	# assemble data into single structure (a)
-	for(tp in time.idx)
-		{	
-			temp			<-	d[(idx[tp]+1):(idx[tp]+numWells),]
-			temp$Time		<-	times[tp]
-			ifelse(tp==time.idx[1], a <- temp, a <- rbind(a,temp))
-		}
+    # assemble data into single structure (a)
+    for(tp in time.idx)
+     {
+      temp <- d[(idx[tp]+1):(idx[tp]+numWells),]
+      temp$Time  <- times[tp]
+      a <- ifelse(tp==time.idx[1], temp, rbind(a,temp))
+     }
 
-	rownames(a)	<-	seq(nrow(a))
+    rownames(a) <- seq(nrow(a))
 
-	# Generate a column for "Well" in the format RCC
-	a[nchar(a$Column)==1,'Column']	<-	paste0('0',a[nchar(a$Column)==1,'Column'])
-	a$Well		<-	paste0(a$Row,a$Column)
-	a$Column	<-	as.integer(a$Column)
+    # Generate a column for "Well" in the format RCC
+    a[nchar(a$Column)==1,'Column'] <- paste0('0',a[nchar(a$Column)==1,'Column'])
+    a$Well  <- paste0(a$Row,a$Column)
+    a$Column <- as.integer(a$Column)
 
-	# calculate log2 cell #
-	a			<-	a[order(a$Well),]
-	a			<-	a[!is.na(a$Cell.count),]
+    # calculate log2 cell #
+    a   <- a[order(a$Well),]
+    a   <- a[!is.na(a$Cell.count),]
 
-	a$nl2		<-	log2norm(a$Cell.count, a$Well, norm_idx=normPos)
-	rownames(a)	<- NULL
-	
-	a
+    a$nl2  <- diprate::log2norm(a$Cell.count, a$Well, norm_idx=normPos)
+    rownames(a) <- NULL
+
+    a
 }
---
+++ diprate/diprate.R
@@ -0,0 +1,20 @@
+#' @author Darren R Tyson, \email{d.tyson@@vanderbilt.edu}
+#' \alias diprate-package
+#' \name diprate
+#' diprate: A package for processing and analyzing drug-induced cell proliferation rates
+#'
+#' The diprate package provides a set of functions for:
+#'  data extraction from various cell count data files,
+#'  estimation of DIP rate from cell count observations over time, and
+#'  analysis of relationships between DIP rate and drug concentration.
+#'
+#'
+#' @section diprate functions:
+#'  \itemize{
+#'      \item{makeDil}
+#'      \itme{many more...}
+#'  }
+#'
+#' @docType package
+#' @name diprate
+NULL
